a8da517c77df5d07f13b6852af63e844b7686170
keyla <keylagomess06@gmail.com>
2025-05-11 19:50:00 -0300

    meus desafios

---
+++ desafios/Crase/desafio.js
@@ -0,0 +1,56 @@
+// Desafio 1: Soma de Dois Números
+// 🧠 Objetivo:
+// Criar uma calculadora simples de soma. O usuário digita dois números e, ao clicar no botão, o resultado da soma aparece na tela.
+
+// 💡 Conceitos Envolvidos:
+// Operadores (+)
+
+// Função JavaScript
+
+// Evento de click
+
+// DOM (acesso a elementos)
+
+function somar(){
+  const numero1 = parseFloat(document.getElementById('num1').value);
+  const numero2 = parseFloat(document.getElementById('num2').value);
+  const resultado = numero1 + numero2;
+  document.getElementById('resultado').textContent = resultado;
+}
+
+
+
+
+
+
+
+
+
+
+//  Desafio 2: Calculadora de Média
+// 🧠 Objetivo:
+// Permitir que o usuário digite três notas. Ao clicar no botão, o JavaScript deve calcular e exibir a média das notas na tela.
+
+// 💡 Conceitos Envolvidos:
+// Operadores aritméticos (+, /)
+
+// Arrays (opcional)
+
+// Evento click
+
+// DOM
+
+// Funções
+
+
+
+
+
+
+
+
+
+
+
+
+
